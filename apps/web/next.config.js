4a07603fd8d64c84d648e52b956fffd89c10c270
fubumaki <fubumaki@gmail.com>
2025-09-07 11:24:26 -0400

    web: transpile workspace packages (@kuyari/db, @kuyari/shared) for Vercel

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['@kuyari/db', '@kuyari/shared'],
   webpack: (config) => {
     config.resolve = config.resolve || {};
     config.resolve.alias = {
